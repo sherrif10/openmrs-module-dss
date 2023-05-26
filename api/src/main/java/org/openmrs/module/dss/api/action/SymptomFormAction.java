0d6c9b2b17a3a62a1a34fb2a4affb7ba538aac05
sherrif10 <sharfspace@gmail.com>
2023-05-26 13:57:37 +0300

    return list of captured obs

--- api/src/main/java/org/openmrs/module/dss/api/action/SymptomFormAction.java
+++ api/src/main/java/org/openmrs/module/dss/api/action/SymptomFormAction.java
@@ -9,7 +9,9 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.CustomFormSubmissionAction;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.apache.commons.logging.Log;
@@ -25,37 +27,41 @@ public class SymptomFormAction implements CustomFormSubmissionAction {
 
     private Log log = LogFactory.getLog(this.getClass());
 
+   
     @Override
     public void applyAction(FormEntrySession formSession) {
         Encounter symptomEncounter = formSession.getEncounter();
-         // we need to add this tag to the symptom form to invoke this class on submit
-        //<PostSubmissionAction class ="org.openmrs.module.dss.api.action.SymptomFormAction"/>
-
+    
         encounterService = Context.getEncounterService();
-
+    
         Optional<Obs> destinationObs = symptomEncounter.getObs().stream()
                 .filter(obs -> obs.getConcept().getConceptId().equals(DESTINATION_CONCEPT_ID)).findFirst();
         if (destinationObs.isPresent()) {
             if (destinationObs.get().getValueText().equals(AIMODEL_DESTINATION)) {
                 Set<Concept> testConcepts = new HashSet<Concept>();
+                List<Obs> capturedObsList = new ArrayList<>();
+    
                 symptomEncounter.getObs().forEach(obs -> {
                     if (obs.getValueCoded() != null) {
                         if (obs.getValueCoded().getConceptClass().getName().equals(TEST_CLASS)) {
                             testConcepts.add(obs.getValueCoded());
                         }
                     }
+                    capturedObsList.add(obs); // Add the observed value to the list of captured observations
                 });
-
+    
                 Encounter newEncounter = new Encounter();
                 newEncounter.setPatient(symptomEncounter.getPatient());
                 newEncounter.setLocation(symptomEncounter.getLocation());
                 newEncounter.setEncounterProviders(symptomEncounter.getActiveEncounterProviders());
                 newEncounter.setEncounterDatetime(symptomEncounter.getEncounterDatetime());
                 newEncounter.setEncounterType(symptomEncounter.getEncounterType());
-
-                log.info(newEncounter);
                 encounterService.saveEncounter(newEncounter);
-                
+    
+                // Return the list of captured observations
+                for (Obs capturedObs : capturedObsList) {
+                    log.info("Captured Obs: " + capturedObs.getConcept().getName().getName());
+                }
             }
         }
     }
