60a82ff33e9a936673092b4eba77462caf835ae4
sherrif10 <sharfspace@gmail.com>
2023-06-01 12:51:55 +0300

    Remove unnecesary code

--- api/src/main/java/org/openmrs/module/dss/DssActivator.java
+++ api/src/main/java/org/openmrs/module/dss/DssActivator.java
@@ -11,16 +11,7 @@ package org.openmrs.module.dss;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
-import org.openmrs.module.dss.api.DssMetadataBundle;
-import org.openmrs.module.metadatadeploy.api.MetadataDeployService;
-import org.openmrs.module.dss.api.HtmlFormsInitializer;
-import org.openmrs.module.dss.api.Initializer;
-
-import java.util.List;
-import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
@@ -33,20 +24,7 @@ public class DssActivator extends BaseModuleActivator {
 	 * @see #started()
 	 */
 	public void started() {
-		log.info("Deploying Metadata");
-		MetadataDeployService metadataDeployService = Context.getService(MetadataDeployService.class);
-		metadataDeployService.installBundle(Context.getRegisteredComponents(DssMetadataBundle.class).get(0));
 		log.info("Starting DSS");
-		
-		for (org.openmrs.module.dss.api.Initializer initializer : getInitializers()) {
-			try {
-				initializer.started();
-			}
-			catch (IOException e) {
-				log.error(e);
-			}
-		}
-		
 	}
 	
 	/**
@@ -54,13 +32,6 @@ public class DssActivator extends BaseModuleActivator {
 	 */
 	public void shutdown() {
 		log.info("Shutdown Dss");
-	}
-	
-	private List<Initializer> getInitializers() {
-		List<Initializer> initializers = new ArrayList<Initializer>();
-		
-		initializers.add(new HtmlFormsInitializer(DssConfig.MODULE_ID));
-		return null;
 	}
 	
 }
