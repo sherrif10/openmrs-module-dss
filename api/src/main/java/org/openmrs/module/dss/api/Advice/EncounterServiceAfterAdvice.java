a59d7de515f26f001f89cb1cd24102fe9b56ff58
sherrif10 <sharfspace@gmail.com>
2023-06-01 07:40:16 +0300

    Testing the after advice

--- api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
+++ api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
@@ -5,36 +5,45 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Encounter;
 import org.openmrs.api.EncounterService;
+import org.openmrs.api.impl.EncounterServiceImpl;
 import org.springframework.aop.AfterReturningAdvice;
+
 import java.util.logging.Logger;
 
 public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
     
     private Log log = LogFactory.getLog(this.getClass());
-    private EncounterService encounterService;
-    
+    private EncounterService encounterService = new EncounterServiceImpl();
+
        @Override
        public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
            Logger.getAnonymousLogger().info("======== in After Advice " + method.getName() + "========");
-           
+
            if (method.getName().equals("saveEncounter") && args[0] != null && returnValue != null) {
                Encounter encounter = (Encounter) returnValue;
                if (encounter != null && encounter.getObs() != null) {
                    encounterService.saveEncounter(encounter);
-                   Logger.getAnonymousLogger().info("Encounter details is not null");
-                   }
-                   Logger.getAnonymousLogger().info("Encounter details is null");
+                   Logger.getAnonymousLogger().info("we are able to save encounter");
 
-                   Logger.getAnonymousLogger().info("Encounter details: " + encounter.toString());
-                     // Log all details of the saved Encounter
-                        log.debug("Saved Encounter Details:");
-                        log.debug("Encounter ID: " + encounter.getId());
-                        log.debug("Encounter Date: " + encounter.getEncounterDatetime());
-                        log.debug("Encounter Location: " + encounter.getLocation());
-                        // Add any additional details you want to log
+                   log.debug("Saved Encounter Details:");
+                   log.debug("Encounter ID: " + encounter.getId());
+                   log.debug("Encounter Date: " + encounter.getEncounterDatetime());
+                   log.debug("Encounter Location: " + encounter.getLocation());
+
+               } else {
+                   Logger.getAnonymousLogger().info("Encounter details is null");
+               }
+           } else if (method.getName().equalsIgnoreCase("unvoidEncounter")) {
+               if (returnValue != null) {
+                 Logger.getAnonymousLogger().info("......un voided encounter..........");
+
+                 }
+                   
            }
-             
        }
-}
+  }
+  
+       
+
 
 
