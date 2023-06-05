53827ca8da1d860b2c3cae4fac1d231c22e28904
sherrif10 <sharfspace@gmail.com>
2023-06-05 18:08:04 +0300

    return list of concepts

--- api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
+++ api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
@@ -3,7 +3,9 @@ package org.openmrs.module.dss.api.Advice;
 import java.lang.reflect.Method;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
 import org.openmrs.Encounter;
+import org.openmrs.Obs;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
 //import org.openmrs.Obs;
@@ -11,6 +13,8 @@ import org.openmrs.api.context.Context;
 //import org.openmrs.api.impl.EncounterServiceImpl;
 import org.springframework.aop.AfterReturningAdvice;
 
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.logging.Logger;
 
 public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
@@ -26,38 +30,27 @@ public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
  * depends on org.openmrs.api.impl.EncounterServiceImpl
  */
 
-       @Override
-       public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
-        Logger.getAnonymousLogger().info("======== in After Advice " + method.getName() + "========");
+ @Override
+ public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+  Logger.getAnonymousLogger().info("======== in After Advice " + method.getName() + "========");
 
-        if (method.getName().equals("saveEncounter") && args[0] != null && returnValue != null) {
-          Encounter encounter = (Encounter) returnValue;
-            if (encounter != null && encounter.getObs() != null) {
-              if (encounterService != null) {
-            
-               //encounterService.saveEncounter(encounter);
-                Logger.getAnonymousLogger().info("we are able to save encounter");
-            
-              } else {
-                Logger.getAnonymousLogger().info("Encounter service is null");
-              }
-            
-                log.debug(".........Saved Encounter Details:.................");
-                log.debug("Encounter ID: " + encounter.getId());
-                log.debug("Encounter Date: " + encounter.getEncounterDatetime());
-                log.debug("Encounter Location: " + encounter.getLocation());
-
-            } else {
-                Logger.getAnonymousLogger().info("Encounter details is null");
-            }
-        } else if (method.getName().equalsIgnoreCase("unvoidEncounter")) {
-            if (returnValue != null) {
-              Logger.getAnonymousLogger().info("......un voided encounter..........");
-              }
-                
-        }
+  if (method.getName().equals("saveEncounter") && args[0] != null && returnValue != null) {
+    Encounter encounter = (Encounter) returnValue;
+      if (encounter != null && encounter.getObs() != null) {
+        if (encounterService != null) {
+          Set<Obs> observations = encounter.getObs();
+          // Iterate through the observations and add them to the ArrayList
+          ArrayList<Obs> observationList = new ArrayList<>();
+          for (Obs obs : observations) {
+            observationList.add(obs);
+            Logger.getAnonymousLogger().info("conceptId = " + obs.getConcept().getId());
+          }
+          Logger.getAnonymousLogger().info("we are able to save encounter");
+        } 
+       }
+     } 
     }
-  }
+}
   
   
        
