647a9e35f17573c47018d96fb084a39cd408e96e
sherrif10 <sharfspace@gmail.com>
2023-06-14 14:21:38 +0300

    Add http request

--- api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
+++ api/src/main/java/org/openmrs/module/dss/api/Advice/EncounterServiceAfterAdvice.java
@@ -1,19 +1,35 @@
 package org.openmrs.module.dss.api.Advice;
 
+
 import java.lang.reflect.Method;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.Concept;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
 import org.springframework.aop.AfterReturningAdvice;
+import java.util.logging.Logger;
+import org.apache.http.HttpEntity;
 
+import org.apache.http.HttpResponse;
+
+import org.apache.http.client.HttpClient;
+
+import org.apache.http.client.methods.HttpPost;
+
+import org.apache.http.entity.ContentType;
+
+import org.apache.http.entity.StringEntity;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import org.apache.http.util.EntityUtils;
 
 import java.util.ArrayList;
 import java.util.Set;
-import java.util.logging.Logger;
 
 public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
 
@@ -23,13 +39,11 @@ public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
   /*
   * (non-Javadoc) * @see
   * org.springframework.aop.AfterReturningAdvice#afterReturning(java.lang.Object,
-  * java.lang.reflect.Method, java.lang.Object[], java.lang.Object)
-  * 
+  * java.lang.reflect.Method, java.lang.Object[], java.lang.Object) 
   * depends on org.openmrs.api.impl.EncounterServiceImpl
   */
   @Override
   public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
-   // Logger.getAnonymousLogger().info("======== in After Advice " + method.getName() + "========");
 
     if (method.getName().equals("saveEncounter") && args[0] != null && returnValue != null) {
       Encounter encounter = (Encounter) returnValue;
@@ -42,14 +56,46 @@ public class EncounterServiceAfterAdvice implements AfterReturningAdvice {
           for (Obs observation : observations) {
             Concept valueCoded = observation.getValueCoded();
             observationList.add(valueCoded);
-            
-        //    Logger.getAnonymousLogger().info("observationList = " + observation.getValueCoded());
+            // Convert the observationList to JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(observationList);
+            Logger.getAnonymousLogger().info("json.............." + json);
           }
-        //  Logger.getAnonymousLogger().info("we are able to save encounter" + observationList);
+
+            // Convert the observationList to JSON
+             ObjectMapper objectMapper = new ObjectMapper();
+             String json = objectMapper.writeValueAsString(observationList);
+             Logger.getAnonymousLogger().info("json.............." + json);
+
+             HttpClient httpClient = HttpClientBuilder.create().build();
+
+             HttpPost request = new HttpPost("http://13.246.5.98:8000/predict"); // Replace with the actual API endpoint URL
+
+             // Set the JSON request body
+             StringEntity requestEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
+             request.setEntity(requestEntity);
+
+             try {
+               // Execute the request
+               HttpResponse response = httpClient.execute(request);
+
+               // Get the response body
+               HttpEntity responseEntity = response.getEntity();
+               String responseBody = EntityUtils.toString(responseEntity);
+
+               // Process the response
+               System.out.println("Response: " + responseBody);
+             }
+             catch (Exception e) {
+               e.printStackTrace();
+             }
+          }
         }
       }
     }
   }
 }
+
+
 
 
